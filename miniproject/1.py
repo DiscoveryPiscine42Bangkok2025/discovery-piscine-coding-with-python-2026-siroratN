b7e6c1f75f8ed525944f0ea04f9c4b9a6a5ff9f5
sirorat nambun <sirorat.n@dcs.premier.co.th>
2026-02-11 22:30:45 +0700

    Update 1.py

--- miniproject/1.py
+++ miniproject/1.py
@@ -8,22 +8,21 @@ def checkmate(check):
     
     # check gird
     for row in grid:
+        # print(row)
         if len(row) != size:
             print("ไม่ใช่จตุรัส")
             return
+        
     # check if there is exactly one king
     king_count = 0
     for row in grid:
         king_count += row.count('K')
-    if king_count > 1:
+    if king_count != 1:
         print("ต้องมี K 1 ตัว")
         return
-    elif king_count == 0:
-        print("ต้องมี K 1 ตัว")
-        return
-    
+
     # check is P, B, R, Q, K
-    valid_pieces = {'P', 'B', 'R', 'Q', 'K', '.'}
+    valid_pieces = ['P', 'B', 'R', 'Q', 'K', '.']
     for row in grid:
         for piece in row:
             if piece not in valid_pieces:
@@ -37,36 +36,32 @@ def checkmate(check):
             print([i, j], grid[i][j])
             if grid[i][j] == 'K':
                 king_pos = [i, j]
-                # return
-    print(f"King position: {king_pos}")
+    # print(f"King position: {king_pos}")
     
     kr, kc = king_pos 
 
     #check king 
+    #ตรง
     king_move = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     for move in king_move:
         r = kr + move[0]
         c = kc + move[1]
         while r >= 0 and r < size and c >= 0 and c < size:
                 piece = grid[r][c]
-                if piece != '.':
-                    if piece == 'R' or piece == 'Q':
-                        print("Success")
-                        return
-                    else:
-                        print(f"เจอ {piece} ขวาง")
-                        break 
+                if piece == 'R' or piece == 'Q':
+                    print("Success")
+                    return
+                elif piece != '.':
+                    break
                 r = r + move[0]
                 c = c + move[1]
                 
-            
+    # แนวทแยง
     pbq_move = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
-    
     for move in pbq_move:
         r = kr + move[0]
         c = kc + move[1]
 
-        # P จะกิน King ได้เมื่อ อยู่เหนือ King 1 
         if r >= 0 and r < size and c >= 0 and c < size:
             if grid[r][c] == 'P' and move[0] == -1: 
                 print("Success")
@@ -79,7 +74,6 @@ def checkmate(check):
                 return
             elif piece != '.':
                 break
-                
             r += move[0]
             c += move[1]
                 
@@ -87,12 +81,11 @@ def checkmate(check):
 
 def main():
     board = """\
-....B.
-...B..
-..K...
-...P..
-......
-......"""
+....
+.P..
+..K.
+....
+"""
     checkmate(board)
 
 main()
